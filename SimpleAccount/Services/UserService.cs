4134d495370f51ade2ba43b704d4b96d53440de0
April John Maraat <aprilmaraat@gmail.com>
2021-02-23 23:30:41 +0800

    Added UserService.cs

---
+++ SimpleAccount/Services/UserService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SimpleAccount.Models;
+
+namespace SimpleAccount.Services
+{
+    public class UserService
+    {
+        private readonly SimpleAccountContext _context;
+
+        public UserService(SimpleAccountContext context)
+        {
+            _context = context;
+        }
+    }
+}
